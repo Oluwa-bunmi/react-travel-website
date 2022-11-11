d2700f1671467f5e33fe5f76fe76f7b6a1e4730f
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-11-11 02:44:40 +0100

    set for toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import { BsPerson, BsSearch } from "react-icons/bs";
 import { AiOutlineClose } from "react-icons/ai";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaYoutube,
+  FaPinterest,
+  FaInstagram,
+} from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div className="flex justify-between items-center h-20 px-4">
       <div>
@@ -22,6 +34,29 @@ const Navbar = () => {
       </div>
       <div className="md:hidden">
         <HiOutlineMenuAlt4 size={20} />
+      </div>
+
+      {/* Dropdown menu on mobile */}
+      <div className="absolute left-[-100%] top-0 bg-gray-100/90 w-full px-4 py-7 flex flex-col">
+        <ul>
+          <h1>BEACHES.</h1>
+          <li className="border-b">Home</li>
+          <li className="border-b">Destinations</li>
+          <li className="border-b">Travel</li>
+          <li className="border-b">View</li>
+          <li className="border-b">Book</li>
+        </ul>
+        <div className="flex flex-col">
+          <button className="my-6">Search</button>
+          <button>Account</button>
+        </div>
+        <div className="flex justify-between my-6">
+          <FaFacebook className="icon" />
+          <FaTwitter className="icon" />
+          <FaYoutube className="icon" />
+          <FaPinterest className="icon" />
+          <FaInstagram className="icon" />
+        </div>
       </div>
     </div>
   );
