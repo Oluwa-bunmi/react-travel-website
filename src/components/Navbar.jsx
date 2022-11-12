246ffb13ca8577f2ebd9fc27ddc2fe4136e437d4
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-11-12 14:17:02 +0100

    done with navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,13 +13,17 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const [logo, setLogo] = useState(false);
   const handleNav = () => {
     setNav(!nav);
+    setLogo(!logo);
   };
   return (
     <div className="flex justify-between items-center h-20 px-4">
       <div>
-        <h1>BEACHES.</h1>
+        <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>
+          BEACHES.
+        </h1>
       </div>
       <ul className="hidden md:flex">
         <li>Home</li>
@@ -29,15 +33,26 @@ const Navbar = () => {
         <li>Book</li>
       </ul>
       <div className="hidden md:flex">
+        <BsSearch className="mr-2" size={20} />
         <BsPerson size={20} />
-        <BsSearch size={20} />
       </div>
-      <div className="md:hidden">
-        <HiOutlineMenuAlt4 size={20} />
+      <div className="z-10 md:hidden" onClick={handleNav}>
+        {nav ? (
+          <AiOutlineClose size={20} className="text-black" />
+        ) : (
+          <HiOutlineMenuAlt4 size={20} />
+        )}
       </div>
 
       {/* Dropdown menu on mobile */}
-      <div className="absolute left-[-100%] top-0 bg-gray-100/90 w-full px-4 py-7 flex flex-col">
+      <div
+        onClick={handleNav}
+        className={
+          nav
+            ? "absolute left-0 top-0 bg-gray-100/90 w-full px-4 py-7 flex flex-col"
+            : "absolute left-[-100%]"
+        }
+      >
         <ul>
           <h1>BEACHES.</h1>
           <li className="border-b">Home</li>
