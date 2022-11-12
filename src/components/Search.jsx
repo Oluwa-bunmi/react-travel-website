5e526758f20e0ba2994eaf92700612cabcc67d5e
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-11-12 16:22:46 +0100

    search section halfway

---
+++ src/components/Search.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { RiCustomerService2Fill } from "react-icons/ri";
+import { MdOutlineTravelExplore } from "react-icons/md";
+
+const Search = () => {
+  return (
+    <div className="max-w-[1240px] grid mx-auto gap-4 px-4 py-16 lg:grid-cols-3 ">
+      <div className="flex flex-col justify-evenly lg:col-span-2">
+        <div>
+          <h2>LUXURY INCLUDED VACATIONS FOR TWO PEOPLE</h2>
+          <p className="py-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quasi
+            obcaecati neque voluptatum ullam quod dolores quis cum, sed
+            veritatis eum repellendus, officia quos quisquam magni. Obcaecati
+            ipsum quasi aliquid, tempore, ratione cum suscipit amet cumque
+            repudiandae nobis aspernatur facilis dolore natus sequi voluptas et
+            distinctio qui, laboriosam quisquam a? Modi optio eveniet mollitia
+            recusandae soluta consequatur architecto, pariatur facilis
+            aspernatur, voluptas incidunt earum voluptatibus! Dolore ipsa
+            repellendus error magnam molestias itaque doloremque iste
+            perspiciatis?
+          </p>
+        </div>
+        <div className="grid sm:grid-cols-2 gap-8 py-4">
+          <div className="flex flex-col lg:flex-row items-center text-center">
+            <button>
+              <RiCustomerService2Fill size={50} />
+            </button>
+            <div>
+              <h3 className="py-2">LEADING SERVICES</h3>
+              <p className="py-1">ALL-INCLUSIVE COMPANY FOR TWO 20 YEARS IN A ROW</p>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-row items-center text-center">
+            <button>
+              <MdOutlineTravelExplore size={50} />
+            </button>
+            <div>
+              <h3>LEADING SERVICES</h3>
+              <p className="py-1">ALL-INCLUSIVE COMPANY FOR TWO 20 YEARS IN A ROW</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>right</div>
+    </div>
+  );
+};
+
+export default Search;
