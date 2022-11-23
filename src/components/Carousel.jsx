98f5b46cf39b97b0d8a918c045264e1c91d559eb
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-11-23 01:20:53 +0100

    carousel section done, now on footer

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Jamaica from "../assets/jamaica.jpg";
 import KeyWest from "../assets/west.jpg";
 import Antigua from "../assets/antigua.jpg";
+import { BsArrowLeftSquareFill, BsArrowRightSquareFill } from "react-icons/bs";
 
 const sliderImages = [
   {
@@ -17,8 +18,26 @@ const sliderImages = [
 
 const Carousel = () => {
   const [slide, setSlide] = useState(0);
+  const length = sliderImages.length;
+
+  const prevSlide = () => {
+    setSlide(slide === length - 1 ? 0 : slide + 1);
+  };
+  const nextSlide = () => {
+    setSlide(slide === 0 ? length - 1  : slide - 1);
+  };
+
   return (
     <div className="max-w-[1240px] mx-auto py-16 px-4 relative flex justify-center items-center">
+      <BsArrowLeftSquareFill
+        onClick={prevSlide}
+        className="absolute text-3xl top-[50%] text-white cursor-pointer left-8 "
+      />
+      <BsArrowRightSquareFill
+        onClick={nextSlide}
+        className="absolute text-3xl top-[50%] text-white cursor-pointer right-8 "
+      />
+
       {sliderImages.map((item, index) => (
         <div className={index === slide ? "opacity-100" : "opacity-0"}>
           {index === slide && (
